4cea9819a20d1829f699ea0e560249312e80ef6b
Chris Alfano <chris@jarv.us>
2017-04-21 16:34:02 -0400

    Render application to app-element element or viewport container

--- sencha-workspace/EmergenceContentEditor/app.js
+++ sencha-workspace/EmergenceContentEditor/app.js
@@ -12,15 +12,25 @@ Ext.application({
         'EmergenceContentEditor.view.main.Main'
     ],
 
-    // The name of the initial view to create. With the classic toolkit this class
-    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
-    // modern toolkit, the main view will be added to the Viewport.
-    //
-    mainView: 'EmergenceContentEditor.view.main.Main'
-	
     //-------------------------------------------------------------------------
     // Most customizations should be made to EmergenceContentEditor.Application. If you need to
     // customize this file, doing so below this section reduces the likelihood
     // of merge conflicts when upgrading to new versions of Sencha Cmd.
     //-------------------------------------------------------------------------
+
+    launch: function() {
+        var viewportEl = Ext.get('app-viewport'),
+            mainView = Ext.create('EmergenceContentEditor.view.main.Main');
+
+        if (viewportEl) {
+            mainView.render(viewportEl);
+        } else {
+            Ext.require('Ext.container.Viewport', function() {
+                Ext.create('Ext.container.Viewport', {
+                    layout: 'fit',
+                    items: mainView
+                });
+            });
+        }
+    }
 });
